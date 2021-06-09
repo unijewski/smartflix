0d571ae8af5713cdc9b95c7b6fc0e4aba2c16f1c
Grzegorz Unijewski <grzegorz@unijewski.pl>
2021-06-09 19:30:08 +0200

    Add a service for creating movies

---
+++ app/services/movies/create.rb
@@ -0,0 +1,36 @@
+module Movies
+  class Create
+    def initialize(title:)
+      @title = title
+    end
+
+    def call
+      movie = Movie.new(movie_attributes.except(:type))
+      movie.movie_type = movie_attributes[:type]
+      movie.ratings = alter_ratings(movie_attributes[:ratings])
+      movie.save!
+    end
+
+    private
+
+    attr_reader :title
+
+    ATTRIBUTES_TO_SKIP = %i[response].freeze
+    private_constant :ATTRIBUTES_TO_SKIP
+
+    def movie_attributes
+      @movie_attributes ||= fetch_data.parsed_response
+                                      .transform_keys(&:underscore)
+                                      .symbolize_keys
+                                      .except(*ATTRIBUTES_TO_SKIP)
+    end
+
+    def fetch_data
+      Apis::Omdb::Movie.new(title: title).call
+    end
+
+    def alter_ratings(ratings_hash)
+      ratings_hash.map { |hash| hash.values.join(': ') }.to_sentence
+    end
+  end
+end
