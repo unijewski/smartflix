f5ffd188fb944e82ac28dbbea28d2ccd67034f39
Grzegorz Unijewski <grzegorz@unijewski.pl>
2021-06-14 15:31:23 +0200

    Call a real service in CreateMovieWorker

--- app/workers/create_movie_worker.rb
+++ app/workers/create_movie_worker.rb
@@ -2,7 +2,7 @@ class CreateMovieWorker
   include Sidekiq::Worker
   sidekiq_options queue: :movies, retry: false
 
-  def perform
-    Movie.create!(title: Faker::Movie.title)
+  def perform(title)
+    Movies::Create.new(title: title).call
   end
 end
