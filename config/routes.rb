3cee697b962f4515eecc23ee7d4e21c6ffd400c2
Grzegorz Unijewski <grzegorz@unijewski.pl>
2021-06-14 15:30:38 +0200

    Define show route path for movies

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :movies, only: %i[show], param: :title
 end
