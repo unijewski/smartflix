2a9282446587d99ce970aaf9f8fa8fb5a6467120
Grzegorz Unijewski <grzegorz@unijewski.pl>
2021-07-15 16:58:55 +0200

    Handle invalid titles in Movies::Create service

--- app/services/movies/create.rb
+++ app/services/movies/create.rb
@@ -7,32 +7,46 @@ module Movies
     end
 
     def call
-      movie = Movie.new(movie_attributes.except(:type))
-      movie.movie_type = movie_attributes[:type]
-      movie.ratings = alter_ratings(movie_attributes[:ratings])
-      movie.save!
+      response_body = fetch_data
+      return unless response_body[:response]
+
+      create_movie(response_body)
     end
 
     private
 
     attr_reader :title
 
-    ATTRIBUTES_TO_SKIP = %i[response].freeze
+    ATTRIBUTES_TO_SKIP = %i[response type].freeze
     private_constant :ATTRIBUTES_TO_SKIP
 
-    def movie_attributes
-      @movie_attributes ||= fetch_data.parsed_response
-                                      .transform_keys(&:underscore)
-                                      .symbolize_keys
-                                      .except(*ATTRIBUTES_TO_SKIP)
-    end
-
     def fetch_data
-      Apis::Omdb::Movie.new(title: title).call
+      data = Apis::Omdb::Movie.new(title: title).call
+      build_response_hash(data)
     end
 
-    def alter_ratings(ratings_hash)
-      ratings_hash.map { |hash| hash.values.join(': ') }.to_sentence
+    def build_response_hash(data)
+      data.parsed_response.tap do |hash|
+        hash.transform_keys!(&:underscore)
+        hash.symbolize_keys!
+        hash[:response] = ActiveModel::Type::Boolean.new.cast(data[:response].downcase)
+      end
+    end
+
+    def create_movie(response)
+      Movie.new(response.except(*ATTRIBUTES_TO_SKIP)).tap do |movie|
+        assign_movie_type(movie, response[:type])
+        alter_ratings(movie, response[:ratings])
+        movie.save!
+      end
+    end
+
+    def assign_movie_type(movie, type)
+      movie.movie_type = type
+    end
+
+    def alter_ratings(movie, ratings_hash)
+      movie.ratings = ratings_hash.map { |hash| hash.values.join(': ') }.to_sentence
     end
   end
 end
