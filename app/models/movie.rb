498e90c30458acb20ad3cd0aa8e1aaf1f54bf561
Grzegorz Unijewski <grzegorz@unijewski.pl>
2021-07-26 13:23:08 +0200

    Add outdated scope to Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,8 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
 
+  scope :outdated, -> { where('updated_at < ?', 48.hours.ago) }
+
   def to_param
     title
   end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,5 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending
+  describe 'scopes' do
+    describe '.outdated' do
+      subject { described_class.outdated }
+
+      let(:movie_1) { create(:movie, updated_at: 2.days.ago) }
+      let(:movie_2) { create(:movie, updated_at: 2.days.ago - 1.minute) }
+      let(:movie_3) { create(:movie, updated_at: 2.days.ago + 1.minute) }
+      let(:movies) { [movie_1, movie_2, movie_3] }
+
+      it do
+        freeze_time do
+          movies
+
+          expect(subject).to eq([movie_2])
+        end
+      end
+    end
+  end
 end
