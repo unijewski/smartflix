d88c19989cd1eac40f78aac103900a0566a0baa7
Grzegorz Unijewski <grzegorz@unijewski.pl>
2021-11-25 14:41:54 +0100

    Add TopMovie.refresh method

--- app/models/top_movie.rb
+++ app/models/top_movie.rb
@@ -4,4 +4,8 @@ class TopMovie < ApplicationRecord
   def readonly?
     true
   end
+
+  def self.refresh
+    Scenic.database.refresh_materialized_view(table_name, concurrently: false, cascade: false)
+  end
 end
