06cdbb54c345b81de3b4c6d9380fa3d1936cb5b5
Grzegorz Unijewski <grzegorz@unijewski.pl>
2021-11-25 14:49:14 +0100

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'movies#index'
+  get '/movies/top', to: 'movies#top'
 
   resources :movies, only: %i[show], param: :title
 end
